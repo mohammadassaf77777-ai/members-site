56e3af03e03b56f67897631e3689d9f8aa6ffaa5
mohammadassaf77777-ai <mohammadassaf77777@gmail.com>
2026-04-03 01:59:37 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -10,11 +10,3 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
-
-// 🔥 هذا أهم جزء
-messaging.onBackgroundMessage(function(payload) {
-  self.registration.showNotification(payload.notification.title, {
-    body: payload.notification.body,
-    icon: '/icon.png'
-  });
-});
